fd191589bf0a6c87635fb6ad498620a741341c58
nackho <nick_ho1011@yahoo.com>
2022-10-28 09:17:43 -0700

    Added update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('goals/', views.goals_index, name='index'),
     path('goals/<int:goal_id>/', views.goals_detail, name='detail'),
     path('goals/create/', views.GoalCreate.as_view(), name='goals_create'),
+    path('goals/<int:pk>/update/', views.GoalUpdate.as_view(), name='goals_update'),
+    path('goals/<int:pk>/delete/', views.GoalDelete.as_view(), name='goals_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Goal
 
 # Create your views here.
@@ -17,7 +17,16 @@ def goals_detail(request, goal_id):
 class GoalCreate(CreateView):
   model = Goal
   fields = ['name', 'category', 'description', 'start_date', 'end_date']
+  success_url = '/goals/'
   
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super().form_valid(form)
+
+class GoalUpdate(UpdateView):
+    model = Goal
+    fields = ['category', 'description', 'start_date', 'end_date']
+
+class GoalDelete(DeleteView):
+    model = Goal
+    success_url = '/goals/'
