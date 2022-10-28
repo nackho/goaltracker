248115b6ce786f6672a00b2ddcd486228dcc2e9d
smkelly1691 <skelly1691@gmail.com>
2022-10-28 13:47:23 -0700

    creates update model

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
+CATEGORIES = (
+  ('P', 'Physical'),
+  ('F', 'Financial'),
+  ('E', 'Education'),
+  ('M', 'Mental'),
+  ('O', 'Other'),
+)
+
 # Create your models here.
 class Goal(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    category = models.CharField(
+        max_length=100,
+        choices=CATEGORIES,
+        default=CATEGORIES[4][0]
+    )
     description = models.TextField(max_length=250)
     start_date = models.DateField()
     end_date = models.DateField()
@@ -17,4 +30,10 @@ class Goal(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'goal_id': self.id})
     
-    
+class Update(models.Model):
+    date = models.DateField()
+    progress_comment = models.TextField(max_length=250)
+    goal = models.ForeignKey(
+        Goal,
+        on_delete=models.CASCADE
+    )  
