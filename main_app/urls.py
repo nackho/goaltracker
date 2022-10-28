8164ef7c0fc8c0d947e65d767bd190591b7befb5
smkelly1691 <skelly1691@gmail.com>
2022-10-27 17:03:18 -0700

    adds goal creation functionality to app

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('goals/', views.goals_index, name='index'),
     path('goals/<int:goal_id>/', views.goals_detail, name='detail'),
+    path('goals/create/', views.GoalCreate.as_view(), name='goals_create'),
 ]
